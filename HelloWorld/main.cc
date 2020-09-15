e3c6df49af6c32e266fefa2edbd9c1b0d6b8214e
inform <inform@CLASS-01.kl9.uspu.ru>
2020-09-15 16:46:53 +0500

    Hello vot tak

---
+++ HelloWorld/main.cc
@@ -0,0 +1,17 @@
+/*
+ * main.cc
+ *
+ *  Created on: 15 сент. 2020 г.
+ *      Author: inform
+ */
+
+
+
+
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+}
