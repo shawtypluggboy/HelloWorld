bf984c2fe1f703976be374e13ca0293870759cfa
inform <inform@CLASS-01.kl9.uspu.ru>
2020-09-15 17:16:43 +0500

    Радиус готов

--- HelloWorld/main.cc
+++ HelloWorld/main.cc
@@ -9,9 +9,24 @@
 
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
+constexpr double Pi = acos(-1.);
 
 int main(){
 	std::cout << "Привет Мир!!" << std::endl;
 
+	std::cout <<"Введите радиус окрыжности: ";
+	double radius;
+	std::cin >> radius;
+	double
+		length = 2*Pi*radius,
+		area = Pi*radius*radius;
+
+	std::cout<<
+			std::fixed<<
+			"Длина окружности = "<< std::setw(9) << std::setprecision(3)<<length<<std::endl<<
+			"Площадь круга    = " << std::setw(9) << std::setprecision(3)<<area<<std::endl;
 	return 0;
 }
